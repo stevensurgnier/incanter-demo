66288c3993c9b9f37b80834e462ee379beaf6eda
ssurgnier <steven.surgnier@simple.com>
2012-10-08 13:13:01 -0700

    cleaned up directory

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to incanter-playground
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/incanter_playground/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns incanter-playground.core-test
-  (:use clojure.test
-        incanter-playground.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
